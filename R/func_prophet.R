b2e7d2ba56afc0445c30e8eb3e1548bbba16d67b
Florian Stracke <flostracke@gmail.com>
2019-04-19 09:12:37 +0200

    added some infunction documentation

--- R/func_prophet.R
+++ R/func_prophet.R
@@ -17,18 +17,23 @@
 #' @return The forecasted values for the series
 forecasts_prophet <- function(data, n_pred, freq, ...) {
 
+  #create the future values for the final result dataframe
   future_dates <- tibble(ds = create_forecasting_dates(data, n_pred))
 
+  #save the current iterate
   current_iterate <- unique(data$iterate)
 
+  #bring the current dataframe to the structure for prophet
   data <- data %>%
-    #bring the current dataframe to the structure for prophet
     dplyr::select(ds = date, y)
 
+  #train the prophet model
   mod_prophet <- prophet::prophet(data, interval.width = 0.95, ...)
 
 
-  forecast <- suppressWarnings(stats::predict(mod_prophet, future_dates)) %>%
+  forecast <-
+    #create the prophet forcasts; suppressWarnings against some strange warning message
+    suppressWarnings(stats::predict(mod_prophet, future_dates)) %>%
     dplyr::mutate(
       key = "prophet",
       iterate = current_iterate
@@ -42,7 +47,7 @@ forecasts_prophet <- function(data, n_pred, freq, ...) {
     ) %>%
     mutate(date = dplyr::pull(future_dates))
 
-
+  #return the wrangeld prophet forecasts
   return(forecast)
 }
 
