4465862abd231f978b3bfb68bdba70d13de00955
Florian Stracke <flostracke@gmail.com>
2019-05-06 13:07:28 +0200

    updated documentation and cleaning up some functions

--- R/func_evaluation.R
+++ R/func_evaluation.R
@@ -7,25 +7,31 @@
 #' This function calculates different metrics for all contained forecasts in a dataframe.
 #' The different models have to be seperated with the help of the key column.
 #'
+#' The metrics are calculated with the help of the yardstick package
+#' (\url{https://github.com/tidymodels/yardstick}).
+#'
 #' @param forecasts The dataframe containing the forecasts.
 #' @param testset The dataframe containing the testset.
-#' @param detailed If True the rmse for each article will be returned.
+#' @param detailed If True the metrics for each article will be returned.
 #' @param metrics A list with the returned metrics. Following metrics are
 #'                possible:
 #'
-#'                - rmse
-#'                - mae
-#'                - rsq
-#'                - mase
-#'                - mape
+#'                \itemize{
+#'                \item {RMSE} Root Mean Squared Error
+#'                \item {MAE} Mean Absolute Error
+#'                \item {RSQ} R Squared
+#'                \item {MASE} Mean Absolute Scaled Error
+#'                \item {MAPE} Mean Absolute Percent Error
+#'                }
+#'
 #'
 #' @examples tf_calc_metrics(sales_forecast, sales_test)
 #'
 #' @export
 #'
-#' @return The calculated rmse for each method.
+#' @return The calculated metrics for each method.
 #'
-tf_calc_metrics <- function(forecasts, testset, metrics = c("rmse", "mae", "rsq", "mase", "mape"), detailed = F) {
+tf_calc_metrics <- function(forecasts, testset, metrics = c("rmse", "mae", "rsq", "mase", "mape"), detailed = FALSE) {
 
   #TODO only calculate the metrics contrained in the metrics parameter
 
@@ -47,127 +53,22 @@ tf_calc_metrics <- function(forecasts, testset, metrics = c("rmse", "mae", "rsq"
 }
 
 
-
-
-
-#' Counts how often each method is the best method against a testset.
-#'
-#' @param forecasts The dataframe with all the forecasts included
-#' @param testset The testset for counting the best method
-#' @param metric The specified metric for choosing the best method
-#' @param plot Plots the count distribution.
-#'
-#' @return The dataframe with the information how many times each method has been the best method
-#' @export
-#'
-#' @examples tf_count_best_method(sales_forecast, sales_test, metric = "rmse", plot = FALSE)
-tf_count_best_method <- function(forecasts, testset, metric = "rmse", plot = FALSE) {
-
-
-  dtl_metrics <- choose_dtl_metric(forecasts, testset, metric)
-
-  #get the minimum rmse for each iterate
-  min_metrics <- dtl_metrics %>%
-    dplyr::group_by(iterate) %>%
-    dplyr::summarise(value = min(value))
-
-  #now innerjoin the min rmse against the rmse of all methods and count how ofteh each method is the best
-  count_best_method <- dtl_metrics %>%
-    dplyr::inner_join(min_metrics, by = c("iterate", "value")) %>%
-    dplyr::count(key)
-
-  if(plot == TRUE) {
-
-    count_best_method %>%
-      ggplot2::ggplot(ggplot2::aes(reorder(key, n), n)) +
-      ggplot2::geom_bar(stat = "identity", fill = "steelblue") +
-      ggplot2::coord_flip() +
-      ggplot2::labs(
-        title = "How often is each method the best forecast?",
-        x = "used forecasting method",
-        y = "# is the best method"
-      )
-
-  } else {
-    return(count_best_method)
-  }
-}
-
-#' Chooses the best method against a validation set from multiple forecasts
-#'
-#' @param forecasts_valid The forecasts against the validation set
-#' @param valid_set The validation set
-#' @param metric The metric which should be used for obtaining the bestmodel
-#'
-#' @return A dataframe
-#' @export
-#'
-#' @examples tf_get_best_method(sales_forecast, sales_test, metric = "rmse")
-tf_get_best_method <- function(forecasts_valid, valid_set, metric = "rmse") {
-
-  #get list with all available models and apply a rank to them in order to
-  #resolve cases when it's not clear which is the best model
-
-  models_row_numers <- forecasts_valid %>%
-    dplyr::select(key) %>%
-    dplyr::distinct() %>%
-    dplyr::arrange(key) %>%
-    dplyr::mutate(n = row_number())
-
-  #calculate the metric for all models and iterates
-  dtl_metric <- choose_dtl_metric(forecasts_valid,valid_set, metric)
-
-  #get the best metric for each iterate
-  best_metric <- dtl_metric %>%
-    dplyr::group_by(iterate) %>%
-    dplyr::summarise(value = min(value))
-
-  #get the info which produces the best forecast
-  best_method <- best_metric %>%
-    dplyr::inner_join(dtl_metric, by = c("iterate", "value")) %>%
-    dplyr::select(iterate, key)
-
-  #add index of the model for choosing always the best model
-  best_method <- best_method %>%
-    dplyr::left_join(models_row_numers, by = "key")
-
-  #get the list which model should be used for which article
-  min_best_method <- best_method %>%
-    dplyr::group_by(iterate) %>%
-    dplyr::summarise(n = min(n))
-
-  #make the model / iterate combinaion unqiue
-  final_best_method <- best_method %>%
-    dplyr::inner_join(min_best_method, by = c("iterate", "n"))
-
-  return(final_best_method)
-
-}
-
-
-
 # -- Internal Functions ----
 
-#' The functions calcualtes one specific metric for all iterates.
+#'  wrapper for calculating metrics with yardstick functions
 #'
-#' @param forecasts The forecast dataframe
-#' @param testset The testset dataframe
-#' @param metric The metric which should be returned
-#'
-#' @return The calculated values for all iterates
+#' It is used in \code{\link{tf_calc_metrics}} for making the function calls for
+#' different metrics modular.
 #'
 #'
-choose_dtl_metric <- function(forecasts, testset, metric = "rmse") {
-
-  metric_var <- rlang::enquo(metric)
-
-  dtl_metric <- tf_calc_metrics(forecasts, testset, detailed = TRUE) %>%
-    dplyr::filter(metric == !!metric_var)
-
-  return(dtl_metric)
-}
-
-
+#' @param forecasts The dataframe containing the forecasts.
+#' @param testset The dataframe containing the testset.
+#' @param func The yardstick function call.
+#' @param detailed If True the metrics for each article will be returned.
+#'
+#' @return The calculated metrics for each method.
+#'
+#' @examples \dontrun{calc_yardstick_metrics(forecasts, testset, func = yardstick::mase, detailed)}
 calc_yardstick_metrics <- function(forecasts, testset, func, detailed) {
 
   # Make the grouping columns to symbols. Then they can be unquoted in the
